c0d02e58f6754eff013ab8a044b2dca4d3cdd79a
Regina1997 <56130620+Regina1997@users.noreply.github.com>
2024-11-05 17:00:46 +0400

    add new buttons

--- bot.js
+++ bot.js
@@ -1,13 +1,14 @@
 require('dotenv').config();
 const { Bot, GrammyError, HttpError, InlineKeyboard, InputFile } = require("grammy");
-const fs = require("fs");
 
 const bot = new Bot(process.env.BOT_API_KEY);
 
 const inlineKeyboard = new InlineKeyboard()
-    .text('Консультация', 'lesson')
-    .text('Онлайн курс', 'course')
-    .text('Бесплатный гайд', 'guide');
+    .text('Консультация 🎓', 'lesson')
+    .row()
+    .text('Онлайн курс 🎧', 'course')
+    .row()
+    .text('Бесплатный гайд 🎁', 'guide');
 
 bot.command("start", async (ctx) =>
     await ctx.reply(`Привет! Меня зовут Хрис Вавржина, автор курса по написанию музыки «The Lazy Producer или как за 1,5 месяца написать трек и не потерять вдохновение». \nПодпишись на <a href="https://t.me/xris_vavrzhina">Telegram-канал</a> «The lazy producer. Все о техно.» \nЧтобы заказать у меня консультацию, получить гайд или купить курс, нажмите одну из кнопок ниже:`,
@@ -18,9 +19,10 @@ bot.command("start", async (ctx) =>
         },
     ));
 
-bot.on("message", (ctx) => ctx.reply("Got another message!"));
+bot.on("message", (ctx) => ctx.reply("Ой, я пока так не умею 🤓"));
 
 bot.callbackQuery('course', async (ctx) => {
+    await ctx.answerCallbackQuery()
     await ctx.reply(`Мой авторский курс  <a href="https://www.thelazyproducer.ru/course">«The Lazy Producer или как за 1,5 месяца написать трек и не потерять вдохновение»</a> с заботой разложен на 4 модуля, чтобы процесс обучения приносил удовольствие. Благодаря четко выстроенной системе, через 1,5 месяца вы получаете готовый трек, и логику, которая поможет вам быстро приходить к своей цели, всегда доводить свои треки до конца и кайфовать в процессе.`,
         {
             parse_mode: 'HTML',
@@ -29,14 +31,26 @@ bot.callbackQuery('course', async (ctx) => {
     )
 });
 
+const consult = new InlineKeyboard()
+    .url('Записаться на консультацию', 'https://t.me/xrisvavrzhina')
+    .row()
+    .text('< Назад в меню', 'back');
+
 bot.callbackQuery('lesson', async (ctx) => {
-    await ctx.reply('Онлайн курс');
+    await ctx.answerCallbackQuery()
+    await ctx.reply('Консультация', 
+        { reply_markup: consult, }
+    );
 });
+
 
 const get_guide = new InlineKeyboard()
     .text('Забрать гайд', 'getguide')
+    .row()
+    .text('< Назад в меню', 'back');
 
 bot.callbackQuery('guide', async (ctx) => {
+    await ctx.answerCallbackQuery()
     await ctx.reply('Бесплатный гайд',
         { reply_markup: get_guide, }
     );
@@ -45,6 +59,7 @@ bot.callbackQuery('guide', async (ctx) => {
 
 bot.callbackQuery('getguide', async (ctx) => {
     // Replace with the path to your PDF file
+    await ctx.answerCallbackQuery('Гайд объемный! Надо подождать 🤏');
     const pdfPath = new InputFile("./files/guide.pdf");
 
     await ctx.replyWithDocument(pdfPath);
@@ -65,9 +80,6 @@ bot.catch((err) => {
 
 bot.api.setMyCommands([
     { command: 'start', description: 'Запуск бота' },
-    { command: 'say_hello', description: 'Получить приветствие' },
-    { command: 'hello', description: 'Получить приветствие' },
-    { command: 'say_hi', description: 'Получить приветствие' },
 ]);
 
 
